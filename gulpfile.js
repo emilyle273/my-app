db4c2e4b05929214dbef0fa306d4ed4468c5fc01
Nhung Le <nhung.le.qn2703@gmail.com>
2021-03-17 19:22:50 +0700

    Update gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,24 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var livereload = require('gulp-livereload');
+var browserSync = require('browser-sync').create();
+
 gulp.task('sass', function() {
   return gulp.src('styles/main.scss') // Gets all files ending with .scss in app/scss and children dirs
     .pipe(sass())
     .pipe(gulp.dest('app/css'))
-    .pipe(livereload())
+    .pipe(browserSync.reload({stream: true}));
 })
 
-gulp.task('html', function() {
-  return gulp.src('/*.html')
-      .pipe(livereload(server))
-});
+gulp.task('watch', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
 
-gulp.task('watch', function(){
-  gulp.watch('styles/**/*.scss', gulp.series(['sass'])); 
-  // Other watchers
+  // copy images/fonts/icons to app
+  gulp.src('./static/**/*').pipe(gulp.dest('./app/static'));
+
+  gulp.watch("./styles/**/*.scss", gulp.series(['sass']))
+  gulp.watch("./styles/**/*.scss").on('change', browserSync.reload);
 })
